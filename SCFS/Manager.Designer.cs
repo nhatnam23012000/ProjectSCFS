2cda7d8758ef776f887de519214d121c4903b6ca
Ngoc <tangocanh99@gmail.com>
2020-06-23 16:20:47 +0700

    Ngoc Anh add Manager's features

---
+++ SCFS/Manager.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace SFCS
+{
+    partial class Manager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.vbReportInfo = new System.Windows.Forms.ListView();
+            this.reportID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.reportOwner = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.reportIncome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.reportOutcome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.reportProfit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Location = new System.Drawing.Point(474, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 48);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Đăng xuất";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(99, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Manager";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Welcome, ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(575, 340);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(336, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 28);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Xem";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(32, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Nhập ngày";
+            // 
+            // vbReportInfo
+            // 
+            this.vbReportInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.reportID,
+            this.reportOwner,
+            this.reportIncome,
+            this.reportOutcome,
+            this.reportProfit});
+            this.vbReportInfo.HideSelection = false;
+            this.vbReportInfo.Location = new System.Drawing.Point(35, 115);
+            this.vbReportInfo.Name = "vbReportInfo";
+            this.vbReportInfo.Size = new System.Drawing.Size(535, 274);
+            this.vbReportInfo.TabIndex = 26;
+            this.vbReportInfo.UseCompatibleStateImageBehavior = false;
+            this.vbReportInfo.View = System.Windows.Forms.View.Details;
+            // 
+            // reportID
+            // 
+            this.reportID.Tag = "";
+            this.reportID.Text = "Mã báo cáo";
+            this.reportID.Width = 100;
+            // 
+            // reportOwner
+            // 
+            this.reportOwner.Tag = "";
+            this.reportOwner.Text = "Tên cửa hàng";
+            this.reportOwner.Width = 150;
+            // 
+            // reportIncome
+            // 
+            this.reportIncome.Text = "Nhập";
+            this.reportIncome.Width = 80;
+            // 
+            // reportOutcome
+            // 
+            this.reportOutcome.Text = "Xuất";
+            this.reportOutcome.Width = 80;
+            // 
+            // reportProfit
+            // 
+            this.reportProfit.Text = "Lợi nhuận";
+            this.reportProfit.Width = 80;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(608, 422);
+            this.Controls.Add(this.vbReportInfo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form6";
+            this.Text = "Form6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView vbReportInfo;
+        private System.Windows.Forms.ColumnHeader reportID;
+        private System.Windows.Forms.ColumnHeader reportOwner;
+        private System.Windows.Forms.ColumnHeader reportIncome;
+        private System.Windows.Forms.ColumnHeader reportOutcome;
+        private System.Windows.Forms.ColumnHeader reportProfit;
+    }
+}
---
+++ SCFS/Manager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SFCS
+{
+    public partial class Manager : Form
+    {
+        public Manager()
+        {
+            InitializeComponent();
+            string[] arr = new string[5];
+            arr[0] = "01/01/2020";
+            arr[1] = "Circle K";
+            arr[2] = "5000$";
+            arr[3] = "12000$";
+            arr[4] = "7000$";
+            ListViewItem l1 = new ListViewItem(arr);
+            vbReportInfo.Items.Add(l1);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
